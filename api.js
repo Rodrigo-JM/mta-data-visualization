7a747762008c2c24ddb836c5ea326d11d0894747
rodrigojm <rodrigomallmann1@gmail.com>
2020-03-27 16:03:29 -0400

    first commit

---
+++ api.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const Mta = require('mta-gtfs');
+const mta = new Mta({
+  key: 'R3mKf9CSXg6LrjjJ79n4H57EjY1apVGl8MiyhRvY', // only needed for mta.schedule() method
+  feed_id: 1                  // optional, default = 1
+});
+
+router.get('/status/:category', async (req, res, next) => {
+    try {
+        const result = await mta.status(req.params.category)
+        res.send(result)
+    } catch(err) {
+        next(err)
+    }
+})
+
+
+module.exports = router
